033a6df8efa63737142a98a5ca086826fcc66973
mrzogy <magbolmrzogy@gmail.com>
2023-05-26 11:04:07 +0300

    Initial Commit

---
+++ Taste Haven/Taste_HavenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Taste_HavenApp.swift
+//  Taste Haven
+//
+//  Created by 3bood on 06/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Taste_HavenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
