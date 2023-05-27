59ea8806a3eb27122b0359abc3ea631ba38aa045
mrzogy <magbolmrzogy@gmail.com>
2023-05-27 09:41:24 +0300

    Finish profile page

---
+++ Taste Haven/Coustoms/CoustomProfile.swift
@@ -0,0 +1,108 @@
+//
+//  CoustomProfile.swift
+//  Taste Haven
+//
+//  Created by 3bood on 07/11/1444 AH.
+//
+
+import SwiftUI
+
+struct CoustomProfile: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack(spacing: 150) {
+                    Label(title: {
+                        Text("Personal Information")
+                    }, icon: {
+                        Image(systemName: "person")
+                            .foregroundColor(Color("green"))
+                    })
+                     
+                    VStack(spacing: 200){
+                        Image(systemName: "chevron.right")
+                    }
+                
+           
+            }
+            .padding(20)
+            HStack(spacing: 195) {
+                    Label(title: {
+                        Text("Payment Card")
+                    }, icon: {
+                        Image(systemName: "creditcard")
+                            .foregroundColor(Color("green"))
+                    })
+                     
+                    VStack(spacing: 200){
+                        Image(systemName: "chevron.right")
+                    }
+                
+           
+                }
+            .padding(20)
+            
+            HStack(spacing: 216) {
+                    Label(title: {
+                        Text("Saved Items")
+                    }, icon: {
+                        Image(systemName: "bookmark")
+                            .foregroundColor(Color("green"))
+                    })
+                     
+                    VStack(spacing: 200){
+                        Image(systemName: "chevron.right")
+                    }
+                
+           
+                }
+            .padding(20)
+            HStack(spacing: 224) {
+                    Label(title: {
+                        Text("My Orders")
+                    }, icon: {
+                        Image(systemName: "handbag")
+                            .foregroundColor(Color("green"))
+                    })
+                     
+                    VStack(spacing: 200){
+                        Image(systemName: "chevron.right")
+                    }
+                
+           
+                }
+            .padding(20)
+            HStack(spacing: 170) {
+                    Label(title: {
+                        Text("Chat and Support")
+                    }, icon: {
+                        Image(systemName: "questionmark.bubble")
+                            .foregroundColor(Color("green"))
+                    })
+                     
+//                    VStack(spacing: 200){
+//                        Image(systemName: "chevron.right")
+//                    }
+                
+                
+           
+                }
+            .padding(20)
+     
+            Label(title: {
+                Text("Log out")
+            }, icon: {
+                Image(systemName: "lock")
+                    .foregroundColor(Color.red)
+            })
+            .padding(20)
+        }
+            
+        }
+    }
+
+
+struct CoustomProfile_Previews: PreviewProvider {
+    static var previews: some View {
+        CoustomProfile()
+    }
+}
--- Taste Haven/View/Prfile.swift
+++ Taste Haven/View/Prfile.swift
@@ -9,8 +9,33 @@ import SwiftUI
 
 struct Prfile: View {
     var body: some View {
-        VStack{
+        NavigationView {
+            VStack(spacing: -10){
+              Image("AAA")
+                    .resizable()
+                    .frame(width: 100,height: 100)
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+                    .frame(width: 200,height: 200)
+                VStack(spacing: 15) {
+                Text("Abdulrazaq Ali")
+                        .bold()
+                Text("@3bood")
+                    Divider()
+                   
+                    VStack() {
+                        CoustomProfile()
+                    }
+                    Spacer()
+                }
+               
+                
             
+                 
+                
+                Spacer()
+            }.navigationTitle("Profile")
+                .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
@@ -20,3 +45,4 @@ struct Prfile_Previews: PreviewProvider {
         Prfile()
     }
 }
+
